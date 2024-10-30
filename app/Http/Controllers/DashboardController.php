b3d3f591800f214e8083714daeaa64b9e625f027
farid0202 <faridmusthofa00@gmail.com>
2024-10-30 10:29:22 +0700

    active

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,8 +6,12 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+    
     public function index()
     {
-        return view();
+        return view('dashboard', [
+            "active" => 'dashboard'
+        ]);
     }
+
 }
--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -1,38 +1,39 @@
 <div class="logo">
-  <a href="https://www.creative-tim.com" class="simple-text logo-mini">
-    <div class="logo-image-small">
-      <img src="../assets/img/solonetputih.png">
-    </div>
-  </a>
-  <a href="https://www.creative-tim.com" class="simple-text logo-normal">
-    Inventory
-  </a>
-</div>
+    <a href="https://www.creative-tim.com" class="simple-text logo-mini">
+      <div class="logo-image-small">
+        <img src="../assets/img/solonetputih.png">
+      </div>
+    </a>
+    <a href="https://www.creative-tim.com" class="simple-text logo-normal">
+      Inventory
+    </a>
+  </div>
+
 <div class="sidebar-wrapper" id="sidebar">
-  <ul class="nav">
-    <li class="{{ request()->is('/') ? 'active' : '' }}">
-      <a href="/">
-        <i class="nc-icon nc-bank"></i>
-        <p>Dashboard</p>
-      </a>
-    </li>
-    <li class="{{ request()->is('tb_barang') ? 'active' : '' }}">
-      <a href="{{ route('barang.index') }}">
-        <i class="nc-icon nc-app"></i>
-        <p>Data Barang</p>
-      </a>
-    </li>
-    <li class="{{ request()->is('tb_user') ? 'active' : '' }}">
-      <a href="{{ url('tb_user') }}">
-        <i class="nc-icon nc-badge"></i>
-        <p>Data User</p>
-      </a>
-    </li>
-    <li class="{{ request()->is('tb_peminjaman') ? 'active' : '' }}">
-      <a href="{{ url('tb_peminjaman') }}">
-        <i class="nc-icon nc-share-66"></i>
-        <p>Data Peminjaman</p>
-      </a>
-    </li>
-  </ul>
-</div>
+    <ul class="nav">
+      <li class="{{ request()->routeIs('dashboard.index') ? 'active' : '' }}">
+        <a href="{{ route('dashboard.index') }}">
+          <i class="nc-icon nc-bank"></i>
+          <p>Dashboard</p>
+        </a>
+      </li>
+      <li class="{{ request()->routeIs('barang.index') ? 'active' : '' }}">
+        <a href="{{ route('barang.index') }}">
+          <i class="nc-icon nc-app"></i>
+          <p>Data Barang</p>
+        </a>
+      </li>
+      <li class="{{ request()->routeIs('user.index') ? 'active' : '' }}">
+        <a href="{{ route('user.index') }}">
+          <i class="nc-icon nc-badge"></i>
+          <p>Data User</p>
+        </a>
+      </li>
+      <li class="{{ request()->routeIs('peminjaman.index') ? 'active' : '' }}">
+        <a href="{{ route('peminjaman.index') }}">
+          <i class="nc-icon nc-share-66"></i>
+          <p>Data Peminjaman</p>
+        </a>
+      </li>
+    </ul>
+  </div>
