45c24def023370c8c516b5f531d23ad79513f7c7
ferdi <ferdinandusdevon23@gmail.com>
2024-11-01 15:12:05 +0700

    PetugasController

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'username',  // Tambahkan ini
+        'level',     // Tambahkan ini
     ];
+    
 
     /**
      * The attributes that should be hidden for serialization.
