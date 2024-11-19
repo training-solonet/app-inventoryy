f38f4a95701262e73651b20696959140f3268662
ferdi <ferdinandusdevon23@gmail.com>
2024-11-19 14:21:53 +0700

    detailadmin

--- app/Http/Controllers/PeminjamanController.php
+++ app/Http/Controllers/PeminjamanController.php
@@ -2,14 +2,85 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Borrow;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class PeminjamanController extends Controller
 {
-    public function index()
+    // Menampilkan daftar peminjaman dengan pagination dan filter tanggal peminjaman
+    public function index(Request $request)
     {
-        return view('tb_peminjaman',[
-            "active" => 'peminjaman'
+        $query = Borrow::query();
+
+        // Filter berdasarkan tanggal mulai
+        if ($request->has('start_date') && $request->start_date) {
+            $query->whereDate('borrow_date', '>=', Carbon::parse($request->start_date)->startOfDay());
+        }
+
+        // Filter berdasarkan tanggal akhir
+        if ($request->has('end_date') && $request->end_date) {
+            $query->whereDate('borrow_date', '<=', Carbon::parse($request->end_date)->endOfDay());
+        }
+
+        // Filter berdasarkan pencarian nama peminjam atau ID peminjaman
+        if ($request->has('search') && $request->search) {
+            $query->where(function ($query) use ($request) {
+                $query->where('borrower_name', 'like', '%' . $request->search . '%')
+                    ->orWhere('borrow_id', 'like', '%' . $request->search . '%');
+            });
+        }
+
+        // Urutkan data terbaru di paling atas
+        $borrows = $query->orderBy('borrow_date', 'desc')->paginate(5);
+
+        return view('tb_peminjaman', compact('borrows'));
+    }
+
+
+
+    // Memperbarui tanggal kembali untuk peminjaman tertentu
+    public function updateReturnDate($id)
+    {
+        $borrow = Borrow::findOrFail($id);
+
+        // Mengatur tanggal kembali ke waktu sekarang
+        $borrow->return_date = now();
+        $borrow->save();
+
+        return response()->json(['return_date' => $borrow->return_date]);
+    }
+
+    // Menampilkan detail peminjaman berdasarkan borrow_id
+    public function showBorrowDetails($borrowId)
+    {
+        // Gunakan eager loading untuk memuat relasi 'borrowItems' dan 'barang'
+        $borrow = Borrow::with('borrowItems.barang')->where('borrow_id', $borrowId)->first();
+
+        // Jika peminjaman tidak ditemukan, tampilkan 404 atau redirect dengan pesan error
+        if (!$borrow) {
+            return redirect()->route('recap')->with('error', 'Peminjaman tidak ditemukan');
+        }
+
+        return view('detailadmin', compact('borrow'));
+    }
+
+
+
+
+
+    // Menampilkan detail peminjaman
+    public function detail($borrow_id)
+    {
+        $borrow = Borrow::with('items')->where('borrow_id', $borrow_id)->first();
+
+        if (!$borrow) {
+            return redirect()->route('recap')->with('error', 'Data peminjaman tidak ditemukan.');
+        }
+
+        return view('detailadmin', [
+            'borrow' => $borrow,
+            'items' => $borrow->items
         ]);
     }
 }
--- resources/views/tb_peminjaman.blade.php
+++ resources/views/tb_peminjaman.blade.php
@@ -1,129 +1,119 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="utf-8" />
-  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-  <title>
-    Peminjaman
-  </title>
-  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
-  <!--     Fonts and icons     -->
-  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
-  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-  <!-- CSS Files -->
-  <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
-  <link href="../assets/css/paper-dashboard.css?v=2.0.1" rel="stylesheet" />
-  <!-- CSS Just for demo purpose, don't include it in your project -->
-  <link href="../assets/demo/demo.css" rel="stylesheet" />
+    <meta charset="utf-8" />
+    <title>Rekap Peminjaman</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/paper-dashboard.css?v=2.0.1') }}" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 
-<body class="">
-  <div class="wrapper ">
-    <div class="sidebar" data-color="white" data-active-color="danger">
+<body>
+    <div class="wrapper">
+        <div class="sidebar" data-color="white" data-active-color="danger">
+            @include('sidebar')
+        </div>
+        <div class="main-panel">
+            @include('navbar')
 
+            <div class="content">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header d-flex justify-content-between align-items-center">
+                                <h4 class="card-title">Rekap Peminjaman</h4>
+                                <a href="{{ route('scan') }}" class="btn btn-primary mr-5">Pinjam Barang</a>
+                            </div>
+                            <div class="card-body">
+                                <form method="GET" action="{{ route('recap') }}" class="mb-4">
+                                    <div class="row">
+                                        <!-- Filter Tanggal Mulai -->
+                                        <div class="col-md-3 mt-3">
+                                            <input type="date" name="start_date" class="form-control" value="{{ request()->input('start_date') }}" placeholder="Tanggal Mulai">
+                                        </div>
 
-      @include('sidebar')
+                                        <!-- Filter Tanggal Akhir -->
+                                        <div class="col-md-3 mt-3">
+                                            <input type="date" name="end_date" class="form-control" value="{{ request()->input('end_date') }}" placeholder="Tanggal Akhir">
+                                        </div>
 
-    </div>
-    <div class="main-panel">
-      <!-- Navbar -->
-      @include('navbar')
-      <!-- End Navbar -->
-      <div class="content">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header d-flex justify-content-between align-items-center">
-                        <h4 class="card-title">Rekap Peminjaman</h4>
-                    </div>
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table">
-                                <thead class="text-primary">
-                                    <th class="text-center">No</th>
-                                    <th class="text-center">Nama Barang</th>
-                                    <th class="text-center">Gambar</th>
-                                    <th class="text-center">Tanggal Peminjaman</th>
-                                    <th class="text-center">Nama Peminjam</th>
-                                    <th class="text-center">Status</th>
-                                    <th class="text-center">Tanggal Kembali</th>
-                                </thead>
-                                <tbody>
-                                    @if(session('borrows'))
-                                        @foreach(session('borrows') as $index => $borrow)
-                                            @php
-                                                $barang = \App\Models\Barang::where('kode_barcode', $borrow->barcode)->first();
-                                            @endphp
-                                            <tr>
-                                                <td class="text-center">{{ $index + 1 }}</td>
-                                                <td class="text-center">{{ $barang ? $barang->nama_barang : 'Tidak Ditemukan' }}</td>
-                                                <td class="text-center">
-                                                    @if ($barang && $barang->gambar)
-                                                        <img src="{{ asset('images/' . $barang->gambar) }}" alt="{{ $barang->nama_barang }}" style="width: 100px; height: auto;">
-                                                    @else
-                                                        Tidak Ada Gambar
-                                                    @endif
-                                                </td>
-                                                <td class="text-center">
-                                                    {{ \Carbon\Carbon::parse($borrow->borrow_date)->format('d-m-Y') }}<br>
-                                                    <small>{{ \Carbon\Carbon::parse($borrow->borrow_date)->format('H:i:s') }}</small>
-                                                </td>
-                                                <td class="text-center">{{ $borrow->borrower_name }}</td>
-                                                <td class="text-center">
-                                                    @if($borrow->status == 'Sedang Dipinjam')
-                                                        <span class="badge badge-sm border border-danger text-uppercase text-white bg-danger">{{ $borrow->status }}</span>
-                                                    @elseif($borrow->status == 'Dikembalikan')
-                                                        <span class="badge badge-sm border border-success text-uppercase text-white bg-success">{{ $borrow->status }}</span>
-                                                    @else
-                                                        <span class="badge badge-sm border border-secondary text-uppercase text-secondary bg-light">{{ $borrow->status }}</span>
-                                                    @endif
-                                                </td>
-                                                <td class="text-center tanggal-kembali">
-                                                    @if ($borrow->return_date)
-                                                        {{ \Carbon\Carbon::parse($borrow->return_date)->format('d-m-Y') }}<br>
-                                                        <small>{{ \Carbon\Carbon::parse($borrow->return_date)->format('H:i:s') }}</small>
-                                                    @else
-                                                        -
-                                                    @endif
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                    @else
-                                        <tr>
-                                            <td colspan="8" class="text-center">Tidak ada data peminjaman.</td>
-                                        </tr>
-                                    @endif
-                                </tbody>
-                            </table>
+                                        <!-- Filter Pencarian -->
+                                        <div class="col-md-4 mt-3">
+                                            <input type="text" name="search" class="form-control" value="{{ request()->input('search') }}" placeholder="Cari berdasarkan Nama Peminjam atau ID Peminjaman">
+                                        </div>
+
+                                        <!-- Tombol Filter -->
+                                        <div class="col-md-2">
+                                            <button type="submit" class="btn btn-outline-primary text-primary">Filter</button>
+                                        </div>
+                                    </div>
+                                </form>
+
+                                <div class="table-responsive">
+                                    <table class="table">
+                                        <thead class="text-primary">
+                                            <th class="text-center">No</th>
+                                            <th class="text-center">No Peminjaman</th>
+                                            <th class="text-center">Nama Peminjam</th>
+                                            <th class="text-center">Tanggal Peminjaman</th> <!-- Kolom untuk tanggal peminjaman -->
+                                            <th class="text-center">Status</th>
+                                            <th class="text-center">Aksi</th>
+                                        </thead>
+                                        <tbody>
+                                            @if($borrows->isNotEmpty())
+                                                @foreach($borrows as $index => $borrow)
+                                                    <tr>
+                                                        <td class="text-center">{{ $borrows->firstItem() + $index }}</td>
+                                                        <td class="text-center">{{ $borrow->borrow_id }}</td>
+                                                        <td class="text-center">{{ $borrow->borrower_name }}</td>
+                                                        <td class="text-center">{{ \Carbon\Carbon::parse($borrow->borrow_date)->format('d-m-Y, H:i:s') }}</td>
+                                                        <td class="text-center">
+                                                            <span class="badge badge-{{ $borrow->borrowItems->where('status', 'Sedang Dipinjam')->isNotEmpty() ? 'danger' : 'success' }}">
+                                                                {{ $borrow->borrowItems->where('status', 'Sedang Dipinjam')->isNotEmpty() ? 'Sedang Dipinjam' : 'Dikembalikan' }}
+                                                            </span>
+                                                        </td>
+                                                        <td class="text-center">
+                                                            <a href="{{ route('detailadmin', ['borrowId' => $borrow->borrow_id]) }}" class="btn btn-info">Lihat Detail</a>
+                                                        </td>
+                                                    </tr>
+                                                @endforeach
+                                            @else
+                                                <tr>
+                                                    <td colspan="6" class="text-center">Tidak ada data peminjaman.</td>
+                                                </tr>
+                                            @endif
+                                        </tbody>
+                                    </table>
+                                </div>
+
+                                <!-- Pagination -->
+                                <div class="d-flex justify-content-center mt-3">
+                                    <!-- Pagination links with Bootstrap pagination style -->
+                                    <ul class="pagination">
+                                        {{ $borrows->links('pagination::bootstrap-4') }}
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+            @include('footer')
         </div>
     </div>
 
-      {{-- footer --}}
-      @include('footer')
-    </div>
-  </div>
-  <!--   Core JS Files   -->
-  <script src="../assets/js/core/jquery.min.js"></script>
-  <script src="../assets/js/core/popper.min.js"></script>
-  <script src="../assets/js/core/bootstrap.min.js"></script>
-  <script src="../assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
-  <!--  Google Maps Plugin    -->
-  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
-  <!-- Chart JS -->
-  <script src="../assets/js/plugins/chartjs.min.js"></script>
-  <!--  Notifications Plugin    -->
-  <script src="../assets/js/plugins/bootstrap-notify.js"></script>
-  <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="../assets/js/paper-dashboard.min.js?v=2.0.1" type="text/javascript"></script><!-- Paper Dashboard DEMO methods, don't include it in your project! -->
-  <script src="../assets/demo/demo.js"></script>
+    <script src="{{ asset('assets/js/core/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/js/core/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/js/core/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/perfect-scrollbar.jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/js/paper-dashboard.min.js?v=2.0.1') }}" type="text/javascript"></script>
 </body>
 
 </html>
